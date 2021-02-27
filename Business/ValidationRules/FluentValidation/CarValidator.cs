72fc93c45fc6d7f0502c9d9d4e186036073adcd7
ÖZLEM TEKİNALP <konuskanozlem@gmail.com>
2021-02-27 20:05:52 +0300

    autofac

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator:AbstractValidator<Car>
+    { 
+        public CarValidator()
+        {
+            RuleFor(c => c.CarId).NotEmpty();
+            RuleFor(c => c.ColorId).NotEmpty();
+            RuleFor(c => c.BrandId).NotEmpty();
+            RuleFor(c => c.Description).MinimumLength(3);
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+        }
+
+    }
+}
